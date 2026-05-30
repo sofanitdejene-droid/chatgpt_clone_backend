029f583a14972503e8682cba41fc1c713d587081
sofanitdejene-droid <sofanitdejene@gmail.com>
2026-05-30 18:15:24 +0300

    Initial commit

---
+++ src/api/chat/controller/chat.controller.js
@@ -0,0 +1,43 @@
+import { createConversationService } from "../service/chat.service.js";
+
+import { getRecentConversationsRow } from "../service/chat.service.js";
+
+
+
+export async function createConversationController(req, res) {
+  try {
+    // request body lay extract yemnaregew question new
+      const { question } = req.body;
+    //   questionachnen le service ensetalun
+
+      const result = await createConversationService(question);
+      res.status(201).json({
+          success: true,
+          message: "Conversation created successfully",
+        //   actual value return enaregalun for frontend
+        data: result,
+      });
+      
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getConversationsController(req, res) {
+
+  try {
+      
+    const result = await getRecentConversationsRow(100);
+    res.status(200).json({
+        success: true,
+        message: "Conversations fetched successfully",
+        data: result,
+    });
+  }
+
+ catch (error){
+         throw error;   
+}
+
+
+};
